ef88c2e50c1166706f3e2dff4add156de2b19c94
Ricardo Herrrera <aherreric@gmail.com>
2021-06-18 01:24:54 -0500

    Consume Service with fixed values

--- RedditList/RedditList/Source/RedditList/Networking/RedditListNetworker.swift
+++ RedditList/RedditList/Source/RedditList/Networking/RedditListNetworker.swift
@@ -9,12 +9,38 @@
 import Foundation
 
 protocol RedditListNetworkerProtocol {
-    func getTop50() // Make async with completion
+    func getTop(completioHandler: @escaping (_ posts: [Posts]?, _ error: Error?) -> Void)
 }
 
 class RedditListNetworker: RedditListNetworkerProtocol {
     
-    func getTop50() {
-        
+    func getTop(completioHandler: @escaping (_ posts: [Posts]?, _ error: Error?) -> Void) {
+        var request = URLRequest(url: URL(string: "https://www.reddit.com/top/.json?limit=2")!)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil,
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                (200 ..< 299) ~= response.statusCode else {
+                completioHandler(nil, error)
+                return
+            }
+
+            if let topList = try? JSONDecoder().decode(TopList.self, from: data) {
+                // we have good data – go back to the main thread
+                let posts = topList.posts
+                for post in posts {
+                    print(post.info)
+                }
+                completioHandler(posts, nil)
+                // everything is good, so we can exit
+                return
+            } else {
+                let customError = NSError(domain: "", code: 999,
+                                          userInfo: [ NSLocalizedDescriptionKey: "Invalid JSon"])
+                completioHandler(nil, customError)
+                return
+            }
+        }.resume()
     }
 }
--- RedditList/RedditList/Source/RedditList/Presenter/RedditListPresenter.swift
+++ RedditList/RedditList/Source/RedditList/Presenter/RedditListPresenter.swift
@@ -9,28 +9,50 @@
 import Foundation
 
 protocol RedditListPresenterProtocol {
-    func getTop50() -> [String] // make async with completion
+    func getTop50()// make async with completion
     func getNextPage(page: Int) -> [String]
     func deleteItems(idList: [String])
-
+    func restartPagination()
 }
 
 class RedditListPresenter: RedditListPresenterProtocol {
 
     let networker: RedditListNetworkerProtocol
+    let storage: RedditStorageProtocol
+    var postsToShow = [Post]()
+    var paging = 10
+    var currentPage = 0
 
-    init(networker: RedditListNetworkerProtocol = RedditListNetworker()) {
+    init(networker: RedditListNetworkerProtocol = RedditListNetworker(),
+         storage: RedditStorageProtocol = RedditStorage()) {
         self.networker = networker
+        self.storage = storage
     }
 
-    func getTop50() -> [String] {
-        return []
+    func getTop50() {
+        networker.getTop { [weak self] (posts, error) in
+            guard error == nil else {
+                // Show error in VC
+                return
+            }
+            guard let self = self else { return }
+            // Filter posts with deleted ones
+            let deletedPosts = self.storage.getDeletedPosts()
+            let filtered = posts?.filter( { !deletedPosts.contains($0.info.postId) } )
+            // Reload table view with posts
+        }
     }
     
     func getNextPage(page: Int) -> [String] {
-        return []
+        // page = 1
+
+        return []//postsToShow[currentPage..currentPage + paging]
     }
-    
+
     func deleteItems(idList: [String]) {
+    }
+
+    func restartPagination() {
+        currentPage = 0
     }
 }
