9c095583bd85b276b6d24ddcbd5a96fc7c94e1e5
Ricardo Herrrera <aherreric@gmail.com>
2021-06-17 21:30:57 -0500

    Create storage base functions

--- RedditList/RedditList/Source/RedditList/Storage/RedditStorageHandler.swift
+++ RedditList/RedditList/Source/RedditList/Storage/RedditStorageHandler.swift
@@ -8,18 +8,52 @@
 
 import Foundation
 
+enum StorageKeys: String {
+    case deleted
+    case read
+}
+
 protocol RedditStorageProtocol { // Use user defaults
-    func getReadPosts() -> [Post]
-    func getDeletedPots() -> [Post]
+    func saveDeletedPost(postId: String)
+    func getReadPosts() -> [String]
+    func getDeletedPosts() -> [String]
+    func isPostRead(postId: String) -> Bool
 }
 
 class RedditStorage: RedditStorageProtocol {
-    func getReadPosts() -> [Post] {
-        return []
+
+    func saveDeletedPost(postId: String) {
+        saveInUserDefaults(postId: postId, for: .deleted)
     }
 
-    func getDeletedPots() -> [Post] {
-        return []
+    func saveReadPost(postId: String) {
+        saveInUserDefaults(postId: postId, for: .read)
+    }
+
+    func saveInUserDefaults(postId: String, for key: StorageKeys) {
+        let userDefaults = UserDefaults.standard
+        var deletedPosts: [String] = userDefaults.object(forKey: key.rawValue) as? [String] ?? [String]()
+        deletedPosts.append(postId)
+        userDefaults.set(deletedPosts, forKey: key.rawValue)
+    }
+
+    func getReadPosts() -> [String] {
+        return getObjectFromUserDefaults(for: .read)
+    }
+
+    func getDeletedPosts() -> [String] {
+        return getObjectFromUserDefaults(for: .deleted)
+    }
+
+    func getObjectFromUserDefaults(for key: StorageKeys) -> [String] {
+        let userDefaults = UserDefaults.standard
+        return userDefaults.object(forKey: key.rawValue) as? [String] ?? [String]()
+    }
+
+    func isPostRead(postId: String) -> Bool {
+        // Evaluate how to avoid calling this every scroll time in tableview
+        let posts = getReadPosts()
+        return posts.contains(postId)
     }
 
 }
